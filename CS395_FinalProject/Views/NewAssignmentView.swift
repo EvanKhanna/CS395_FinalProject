efa7605b4fc91d281f70953ebe6375c3d34d979d
EvanKhanna <162855408+EvanKhanna@users.noreply.github.com>
2024-10-22 14:43:24 -0400

    Creation of Bottom Tab bar and New Assignment View

---
+++ CS395_FinalProject/Views/NewAssignmentView.swift
@@ -0,0 +1,18 @@
+//
+//  NewAssignmentView.swift
+//  CS395_FinalProject
+//
+//  Created by Evan Khanna on 10/22/24.
+//
+
+import SwiftUI
+
+struct NewAssignmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewAssignmentView()
+}
