eb76c18fd97da22cbe4d465998f2ad65b5bdfefd
EvanKhanna <162855408+EvanKhanna@users.noreply.github.com>
2024-10-27 22:41:58 -0400

    Added Filter List Option and swipe to delete

--- CS395_FinalProject/Models/AssignmentViewModel.swift
+++ CS395_FinalProject/Models/AssignmentViewModel.swift
@@ -10,6 +10,20 @@ import Foundation
 class AssignmentViewModel: ObservableObject {
     @Published var assignments: [Assignment] = Assignment.getAssignments()
     
+    init() {
+            loadAssignments()
+        }
+
+    func loadAssignments() {
+        // Fetches assignments from storage and updates the assignments array
+        assignments = Assignment.getAssignments()
+    }
+    
+    func deleteAssignment(_ assignment: Assignment) {
+            assignments.removeAll { $0.id == assignment.id }
+            saveAssignments()
+        }
+    
     func addOrUpdateAssignment(_ assignment: Assignment) {
             if let index = assignments.firstIndex(where: { $0.id == assignment.id }) {
                 assignments[index] = assignment
--- CS395_FinalProject/Views/AssignmentListView.swift
+++ CS395_FinalProject/Views/AssignmentListView.swift
@@ -7,28 +7,72 @@
 
 import SwiftUI
 
-struct AssignmentListView: View {
-    @EnvironmentObject var assignmentViewModel: AssignmentViewModel  // Shared ViewModel
+enum AssignmentFilter: String, CaseIterable, Identifiable {
+    case course = "Course"
+    case type = "Type"
+    case dueDate = "Due Date"
+    
+    var id: String { self.rawValue }
+}
 
+struct AssignmentListView: View {
+    @EnvironmentObject var assignmentViewModel: AssignmentViewModel
+    @State private var selectedFilter: AssignmentFilter = .dueDate  // Default filter
+    
     var body: some View {
         NavigationView {
-            List(assignmentViewModel.assignments) { assignment in
-                NavigationLink(
-                    destination: AssignmentDetailView(assignment: $assignmentViewModel.assignments[getIndex(of: assignment)])
-                ) {
-                    AssignmentRow(assignment: assignment)
+                    List {
+                        ForEach(filteredAssignments) { assignment in
+                            NavigationLink(
+                                destination: AssignmentDetailView(assignment: $assignmentViewModel.assignments[getIndex(of: assignment)])
+                            ) {
+                                AssignmentRow(assignment: assignment)
+                            }
+                        }
+                        .onDelete(perform: deleteAssignment)  // Enable swipe-to-delete
+                    }
+            .navigationTitle("Assignments")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu {
+                        ForEach(AssignmentFilter.allCases) { filter in
+                            Button(action: { selectedFilter = filter }) {
+                                Text("Sort by \(filter.rawValue)")
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "line.horizontal.3.decrease.circle")
+                    }
                 }
             }
-            .navigationTitle("Assignments")
+        }
+        .onAppear {
+            assignmentViewModel.loadAssignments()  // Reload assignments when view appears
         }
     }
-
+    
+    private var filteredAssignments: [Assignment] {
+        switch selectedFilter {
+        case .course:
+            return assignmentViewModel.assignments.sorted { $0.course < $1.course }
+        case .type:
+            return assignmentViewModel.assignments.sorted { $0.assignmentType < $1.assignmentType }
+        case .dueDate:
+            return assignmentViewModel.assignments.sorted { $0.dueDate < $1.dueDate }
+        }
+    }
+    
+    private func deleteAssignment(at offsets: IndexSet) {
+            for index in offsets {
+                let assignment = filteredAssignments[index]
+                assignmentViewModel.deleteAssignment(assignment)
+            }
+        }
+    
     private func getIndex(of assignment: Assignment) -> Int {
         return assignmentViewModel.assignments.firstIndex(where: { $0.id == assignment.id }) ?? 0
     }
 }
-
-
 
 struct AssignmentRow: View {
     var assignment: Assignment
@@ -48,6 +92,9 @@ struct AssignmentRow: View {
     }
 }
 
-#Preview {
-    AssignmentListView()
+struct AssignmentListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssignmentListView()
+            .environmentObject(AssignmentViewModel())
+    }
 }
